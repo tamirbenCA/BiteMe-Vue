59c3dfd1182518756d3947d3fba8f770a95909b0
Ben Tamir <tamirben@gmail.com>
2017-12-26 10:58:05 +0200

    db connect fixed

--- server/server-full.js
+++ server/server-full.js
@@ -47,10 +47,9 @@ const io = require('socket.io')(http);
 
 
 function dbConnect() {
-
 	return new Promise((resolve, reject) => {
 		// Connection URL
-		var url = 'mongodb://localhost:27017/mayNer';
+		var url = 'mongodb://tamirben:tamirben@ds133127.mlab.com:33127/byte-me';
 		// Use connect method to connect to the Server
 		mongodb.MongoClient.connect(url, function (err, db) {
 			if (err) {
@@ -58,7 +57,7 @@ function dbConnect() {
 				reject(err);
 			}
 			else {
-				//cl('Connected to DB');
+				cl('Connected to DB');
 				resolve(db);
 			}
 		});
@@ -95,6 +94,7 @@ app.get('/data/tags', function (req, res) {
 
 // GETs a list
 app.get('/data/:objType', function (req, res) {
+	// console.log()
 	const objType = req.params.objType;
 	var query = getBasicQueryObj(req);
 	dbConnect().then(db => {
