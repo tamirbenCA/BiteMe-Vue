febf8ac3efe83b0846fc6f8f4791b32a910cdd3f
May <may.schiller@gmail.com>
2018-01-03 00:39:52 +0200

    may

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,27 +1,30 @@
 <template>
   <!-- <div class="container"> -->
   <!-- <div class="msg">
-                <h1>HOME COOKED</h1>
-                <h1>HEALTHY MEALS</h1>
-                <h1>DELIVERED TO YOU</h1>
-              </div> -->
+                    <h1>HOME COOKED</h1>
+                    <h1>HEALTHY MEALS</h1>
+                    <h1>DELIVERED TO YOU</h1>
+                  </div> -->
   <!-- <div class="frnt-img">
-              <img src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514927973/food-eating-potatoes-beer-8313_xwqvz3.jpg" alt="">
-            </div> -->
+                  <img src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514927973/food-eating-potatoes-beer-8313_xwqvz3.jpg" alt="">
+                </div> -->
   <div class="imgs-btm">
     <div class="animated bounceInRight"> <img class="img-btm" src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514928793/pexels-photo-566345_l9uouq.jpg" alt=""></div>
-<div class="animated bounceInRight"><img class="img-btm" src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514928793/food-prawn-asian_mbxnej.jpg" alt=""></div>
-    
-   <div class="animated bounceInRight"><img class="img-btm" src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514928793/pexels-photo-76093_xlkq0h.jpg" alt=""></div>
-    
+    <div class="animated bounceInRight"><img class="img-btm" src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514928793/food-prawn-asian_mbxnej.jpg" alt=""></div>
+
+    <div class="animated bounceInRight">
+      <img class="img-btm" src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514928793/pexels-photo-76093_xlkq0h.jpg" alt="">
+      <img src="../assets/icons8-new-40.png" style="position:relative; top:0;left: -70px;width:40px;height:40px">
+
+    </div>
 
     <div class="animated bounceInRight">
       <img class="img-btm" src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514928795/pexels-photo-265393_c1j4rl.jpg" alt="">
-      <!-- <i style="position:absolute;font-size:30px;" class="fa fa-user-o" aria-hidden="true"></i> -->
     </div>
     <div class="animated bounceInRight">
       <img class="img-btm" src="http://res.cloudinary.com/dl58rg6j8/image/upload/v1514928795/sushi-japan-soya-rice-681586_axfola.jpg" alt="">
-      <!-- <i style="position:absolute;font-size:30px;" class="fa fa-user-o" aria-hidden="true"></i> -->
+          <img src="../assets/icons8-new-40.png" style="position:relative; top:0;left: -70px;width:40px;height:40px;">
+
     </div>
   </div>
 
@@ -44,6 +47,11 @@ export default {
   justify-content: center;
   align-items: center;
   height: 400px;
+  cursor: pointer;
+}
+
+.img-btm:hover {
+  box-shadow: 1px 2px 6px 0px black;
 }
 
 .img-btm {
