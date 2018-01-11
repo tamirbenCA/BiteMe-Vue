525dc3bab64cee16bf2c71a8ce13ba204659cc56
May <may.schiller@gmail.com>
2018-01-11 14:27:25 +0200

    may

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,6 +1,13 @@
 <template>
   <section class="fr-page-cnt">
     <div class="box" v-if="pageReady">
+      <div class="box-top">
+        <router-link class="rtl" to="/meals">
+          Explore The Marketplace
+          <i class="fa fa-search" aria-hidden="true"></i>
+        </router-link>
+
+      </div>
       <p class="gt-start">Get Started With Our Best Sellers</p>
       <div class="imgs-container">
         <div class="imgs-btm" v-for="(meal, idx) in meals" :key="idx">
@@ -42,6 +49,7 @@ export default {
     }
   },
   methods: {
+
     showDetails(meal) {
       this.$router.push('/itemdetails/' + meal._id);
     }
@@ -51,6 +59,26 @@ export default {
 </script>
 
 <style>
+.box-top {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 30%;
+  margin: auto;
+}
+
+.fa-search {
+  color: white;
+  font-size: 30px;
+}
+
+.rtl {
+  text-decoration: none;
+  font-size: 30px;
+  margin-bottom: 0;
+  color: #322f2f;
+}
+
 .new-icn {
   position: relative;
   top: -270px;
@@ -71,8 +99,8 @@ export default {
 }
 
 .gt-start {
-  text-align: left;
-  font-size: 30px;
+  /* text-align: left; */
+  font-size: 24px;
   margin-bottom: 0;
   color: #322f2f;
 }
@@ -80,7 +108,7 @@ export default {
 .box {
   display: flex;
   flex-direction: column;
-  height: 500px;
+  height: 530px;
   justify-content: space-between;
   margin-top: 50px;
 }
@@ -98,6 +126,7 @@ export default {
 .imgs-container {
   display: flex;
   flex-direction: row;
+  margin-top: 20px;
 }
 
 .img-btm:hover {
