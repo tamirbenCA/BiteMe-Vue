dee4a0e7e30da3dc5fb359ae74589733be768467
Ben Tamir <tamirben@gmail.com>
2018-01-27 00:04:39 +0200

    ben

--- old-backup/EventBusService.js
+++
@@ -1,6 +0,0 @@
-export const SHOW_MSG = 'show-msg';
-export const LOGIN_SUCCESS = 'succesful-login';
-
-var bus = new Vue();
-
-export default bus;
--- old-backup/server-old/users-jason.js
+++
@@ -1,1 +0,0 @@
-db.getCollection('user').find({})
